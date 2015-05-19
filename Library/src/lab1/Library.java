e772e508efddd811027a7b0acb6f6f8cfbda8eef
Winnie <Winnie@Winnie-PC>
2015-05-19 15:04:51 -0400

    Completed Step Two

--- Library/src/lab1/Library.java
+++ Library/src/lab1/Library.java
@@ -5,8 +5,79 @@ package lab1;
  * @author <a href="mailto:rkhatchadourian@citytech.cuny.edu">Raffi Khatchadourian</a>.
  */
 public class Library {
-    // TODO: Add the missing implementation to this class
-
+    String address;
+    Book[] collection = {};
+    
+    public Library(String location) {
+        address = location;
+    }
+    
+    public void addBook(Book book) {
+        Book[] add = new Book[collection.length + 1];
+        for (int i = 0; i < collection.length; i++) {
+            add[i] = collection[i];
+        }
+        add[collection.length] = book;
+        collection = add;
+    }
+    
+    public void borrowBook (String title) {
+        boolean inCollection = false;
+        for (int i = 0; i < collection.length; i++) {
+            if (collection[i].getTitle().equals(title)) {
+                if (collection[i].isBorrowed()) {
+                    System.out.println("Sorry, this book is already borrowed.");
+                    inCollection = true;
+                }
+                else {
+                    collection[i].rented();
+                    System.out.println("You successfully borrowed " + title);
+                    inCollection = true;
+                }
+            }
+        }
+        if (!(inCollection)) {
+            System.out.println("Sorry, this book is not in our catalog.");
+        }
+    }
+    
+    public void returnBook (String title) {
+        boolean inCollection = false;
+        for (int i = 0; i < collection.length; i++) {
+            if (collection[i].getTitle().equals(title)) {
+                if (collection[i].isBorrowed()) {
+                    System.out.println("You successfully returned " + title);
+                    collection[i].returned();
+                    inCollection = true;
+                }
+            }
+        }
+        if (!(inCollection)) {
+            System.out.println("Sorry, this book is not in our catalog.");
+        }
+    }
+    
+    public static void printOpeningHours() {
+        System.out.println("Libraries are open daily from 9am to 5pm.");
+    }
+    
+    public void printAvailableBooks() {
+        boolean empty = true;
+        for (int i = 0; i < collection.length; i++) {
+            if (!(collection[i].isBorrowed())) {
+                System.out.println(collection[i].getTitle());
+                empty = false;
+            }
+        }
+        if (empty) {
+            System.out.println("No book in catalog");
+        }
+    }
+    
+    public void printAddress() {
+        System.out.println(address);
+    }
+    
     public static void main(String[] args) {
         // Create two libraries
         Library firstLibrary = new Library("10 Main St.");
